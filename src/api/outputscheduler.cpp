fb3c853753b0a28eeef7ce601b206af8edb77eb9
motionx77 <d1g1taL2>
2015-08-22 10:54:38 +0300

    write operations are now queued up at the socket level

---
+++ src/api/outputscheduler.cpp
@@ -0,0 +1,7 @@
+#include "oscheduler.h"
+
+oscheduler::oscheduler()
+{
+
+}
+
---
+++ src/api/outputscheduler.h
@@ -0,0 +1,11 @@
+#ifndef OSCHEDULER_H
+#define OSCHEDULER_H
+
+
+class oscheduler
+{
+public:
+    oscheduler();
+};
+
+#endif // OSCHEDULER_H
