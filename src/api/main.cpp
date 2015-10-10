be572bb554a6c3c3061bde798ff29d8ef93add37
Vladimir Voinea <voineavladimir@gmail.com>
2015-10-11 00:12:35 +0300

    fixed a path error

--- src/api/main.cpp
+++ src/api/main.cpp
@@ -65,9 +65,9 @@ int main() {
         Settings settings;
 #ifndef __arm__
         settings.root_path =
-                ".";
+                "/media/vladimir/Seagate Expansion Drive/server";
 #else
-        settings.root_path = ".";
+        settings.root_path = "/mnt/exthdd/server";
 #endif
         settings.max_connections = 1000;
         s.setSettings(settings);
